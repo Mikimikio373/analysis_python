ced6e67aa7d399fea1c32c5d26ad927896d84e8a
Mikimikion <minami.flab@gmail.com>
2023-08-11 22:49:41 +0900

    renameの練習

--- hts2/test.py
+++ hts2/test.py
@@ -1,1 +1,14 @@
-print('hello world')
+import os
+
+basepath = 'Q:/minami/20230811_dip/1-7/0.5um'
+os.chdir(basepath)
+current_dir = os.getcwd()
+print('directory changed, current path: {}'.format(current_dir))
+count = 8
+for i in range(400):
+    tar_file_name = '20230811T170{:03}'.format(i)
+    if not os.path.exists(tar_file_name):
+        continue
+
+    os.rename(tar_file_name, '{}'.format(count))
+    count += 1
