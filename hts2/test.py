72c891410468fb022ef9f67aacb1632894449af3
Mikimikion <minami.flab@gmail.com>
2023-08-11 23:06:27 +0900

    1-7の0.5umピッチ駆動50回分のpiezo-zをループ動作

--- hts2/test.py
+++ hts2/test.py
@@ -1,21 +1,14 @@
 import json
+import subprocess
 import sys
 import os
 
 import numpy as np
 
-if not len(sys.argv) == 2:
-    sys.exit('command line error. please input \"basepath\"')
+basepath = 'Q:/minami/20230811_dip/1-7/0.5um'
 
-basepath = sys.argv[1]
-
-z_list = []
 for i in range(1, 51):
-    tar_json = os.path.join(basepath, '{}'.format(i), 'image.json')
-    with open(tar_json, 'r') as f:
-        j = json.load(f)
-
-    z_list.append(float(j['Images'][0]['stagez']))
-
-out_csv = os.path.join(basepath, 'stagez.csv')
-np.savetxt(out_csv, np.asarray(z_list), delimiter=',')
+    tar_dir = os.path. join(basepath, '{}'.format(i))
+    command = 'python piezo-z.py {}'.format(tar_dir)
+    # print(command)
+    subprocess.run(command, shell=True)
