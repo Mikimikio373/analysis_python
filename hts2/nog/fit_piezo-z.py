d0e8425467d159904cec8f4758975dfbfa3adf85
Mikimikion <minami.flab@gmail.com>
2023-09-06 15:16:38 +0900

    引数の受け取り方を変更

--- hts2/nog/fit_piezo-z.py
+++ hts2/nog/fit_piezo-z.py
@@ -4,13 +4,21 @@ import json
 import sys
 
 import numpy as np
+import pandas as pd
 from matplotlib import pyplot as plt
 
-if not len(sys.argv) == 2:
-    sys.exit('command line error. please input \"basepath\"')
+if len(sys.argv) == 2:
+    basepath = sys.argv[1]
+    starskip = 0
+    endskip = 0
+elif len(sys.argv) == 4:
+    basepath = sys.argv[1]
+    starskip = int(sys.argv[2])
+    endskip = int(sys.argv[3])
+else:
+    sys.exit('use error. please input \"basepath\", (\"start skip num\", \"end skip num\")')
 
-basepath = sys.argv[1]
-
+print(basepath)
 json_path = os.path.join(basepath, 'image.json')
 with open(json_path, 'r') as f:
     j = json.load(f)
@@ -31,9 +39,13 @@ for i in range(len(skip_list)):
     tmp1 = piezo_list.pop(skip_list[i])
     x = x[x != skip_list[i]]
 
-delete_num = -8 - len(skip_list)
-x_fix = x[2:delete_num]
-piezo_list_fix = piezo_list[2:delete_num]
+delete_num = -endskip - len(skip_list)
+if delete_num == 0:
+    x_fix = x
+    piezo_list_fix = piezo_list
+else:
+    x_fix = x[starskip:delete_num]
+    piezo_list_fix = piezo_list[starskip:delete_num]
 
 p, cov = np.polyfit(x_fix, piezo_list_fix, 1, cov=True)
 a = p[0]
@@ -49,18 +61,21 @@ plt.plot(x, piezo_list, '-x')
 plt.plot(x, y_fit, '-', label='y = {:.3g} $\pm$ {:.3g} x + ({:.3g} $\pm$ {:.3g})'.format(a, a_err, b, b_err))
 plt.xlabel('number of picture')
 plt.ylabel('z [um]')
-plt.axvline(x=x[2], c='r')
-plt.axvline(x=x[-8], c='r')
+plt.axvline(x=x_fix[0], c='r')
+plt.axvline(x=x_fix[-1], c='r')
 plt.grid()
 plt.legend()
 plt.savefig(os.path.join(basepath, 'image_json.png'), dpi=300)
 # plt.show()
 plt.clf()
 
-sigma_y = np.sqrt(1/(len(x_fix) - 2) * np.sum([(a*x1+b-y1)**2 for x1, y1 in zip(x_fix, piezo_list_fix)]))
+sigma_y = np.sqrt(1/(len(x_fix)) * np.sum([(a*x1+b-y1)**2 for x1, y1 in zip(x_fix, piezo_list_fix)]))
 print(sigma_y)
-y_err = np.asarray(piezo_list) - np.asarray(y_fit)
-plt.hist(y_err[2:-8])
+if delete_num == 0:
+    y_err = np.asarray(piezo_list) - np.asarray(y_fit)
+else:
+    y_err = np.asarray(piezo_list_fix) - np.asarray(y_fit[starskip:delete_num])
+plt.hist(y_err[2:-8], bins=50, range=(-0.5, 0.5))
 plt.xlabel('data - fit [um]')
 plt.ylabel('entries')
 plt.title('σ = {:.3g}'.format(sigma_y))
@@ -76,3 +91,7 @@ with open(os.path.join(basepath, 'fitdata.csv'), 'w', newline='') as f:
     writer.writerow(['b_err', b_err])
     writer.writerow(['fit err', sigma_y])
 
+z_data_df = pd.DataFrame()
+z_data_df['piezo z'] = piezo_list
+z_data_df['fit z'] = y_fit
+z_data_df.to_csv(os.path.join(basepath, 'piezo_z.csv'), index=False)
