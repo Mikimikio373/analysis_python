84fa4f9e15faa4caf9bc87ce52d8a2e3296e3706
Mikimikion <minami.flab@gmail.com>
2023-09-07 15:09:39 +0900

    ヒットピクセルを計算

--- hts2/test.py
+++ hts2/test.py
@@ -1,31 +1,14 @@
 import cv2
-import cupy as cp
 import numpy as np
-from matplotlib import pyplot as plt
-import time
 
-img_path = 'Q:/minami/graine_u4/PL089/L0_VX0000_VY0000/L0_VX0000_VY0000_10.png'
+# img_path = 'Q:/minami/graine_u4/PL089/L0_VX0009_VY0043_thr1211_dil/L0_VX0009_VY0043_2_thr2.png'
+img_path = 'Q:/minami/graine_u4/PL089/L0_VX0009_VY0043_cubicthr1211_dil/L0_VX0009_VY0043_2_thr2.png'
+# img_path = 'Q:/minami/graine_u4/PL089/L0_VX0009_VY0043_thr1211/L0_VX0009_VY0043_2_thr.png'
+# img_path = 'Q:/minami/graine_u4/PL089/L0_VX0009_VY0043_cubicthr1211/L0_VX0009_VY0043_2_thr.png'
 
-time_list = []
-start = time.perf_counter()
 img = cv2.imread(img_path, 0)
-end = time.perf_counter()
-time_list.append(end - start)
+num = np.count_nonzero(img)
+print(num)
 
-start = time.perf_counter()
-f = np.fft.fft2(img)
-end = time.perf_counter()
-time_list.append(end - start)
-
-fshift = np.fft.fftshift(f)
-
-spectrum = np.abs(fshift)
-spectrum_log = np.log(spectrum)
-x = np.concatenate([np.arange(-1023, 1), np.arange(1024)])
-y = np.concatenate([np.arange(-543, 1), np.arange(544)])
-xx, yy = np.meshgrid(x, y)
-fig, ax = plt.subplots()
-im = ax.contourf(xx, yy, spectrum_log)
-fig.colorbar(im, ax=ax)
-plt.show()
-print(time_list)
+hit_per = num / (len(img) * len(img[0])) * 100
+print(hit_per)
