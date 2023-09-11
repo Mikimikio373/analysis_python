125d9ad95914b62ae735bed3806f51bdc4e31609
Mikimikion <minami.flab@gmail.com>
2023-09-11 19:49:40 +0900

    軽微な変更

--- hts2/plot_3D.py
+++ hts2/plot_3D.py
@@ -1,3 +1,6 @@
+import os
+import sys
+
 import pandas as pd
 import numpy as np
 from matplotlib import pyplot as plt
@@ -5,12 +8,38 @@ from io import BytesIO
 from PIL import Image
 from mpl_toolkits.mplot3d import axes3d
 
-csv_path = 'Q:/minami/20230910_nog2/xyz1.csv'
-out_gif = 'Q:/minami/20230910_nog2/z_1.gif'
+basepath = 'Q:/minami/20230911_nog_NRKR_tilt'
+csv_path = os.path.join(basepath, 'xyz_fit.csv')
+surf_fit = os.path.join(basepath, 'fit_surf.csv')
 
-a = -2.1282
-b = 1.16567
-c = -5.28872
+if not len(sys.argv) == 3:
+    sys.exit('please input mode')
+mode = int(sys.argv[1])
+fit_mode = int(sys.argv[2])
+if fit_mode != 0 and fit_mode != 1:
+    sys.exit('fit mode must be 1/0')
+if mode == 0:
+    if fit_mode == 1:
+        out_gif = os.path.join(basepath, 'z0_fix.gif')
+    else:
+        out_gif = os.path.join(basepath, 'z0.gif')
+elif mode == 1:
+    if fit_mode == 1:
+        out_gif = os.path.join(basepath, 'z1_fix.gif')
+    else:
+        out_gif = os.path.join(basepath, 'z1.gif')
+elif mode == 2:
+    if fit_mode == 1:
+        out_gif = os.path.join(basepath, 'z_fix.gif')
+    else:
+        out_gif = os.path.join(basepath, 'z.gif')
+else:
+    sys.exit('mode error')
+
+fit_df= pd.read_csv(surf_fit)
+a = fit_df['a'][1]
+b = fit_df['b'][1]
+c = fit_df['c'][1]
 
 def calc_fit_z(x, y):
     z = a * x + b * y + c
@@ -27,40 +56,45 @@ y1 = []
 z1 = []
 zerr1 = []
 for i in range(len(df)):
-    if df['flag'][i] == 0:
-        x0.append(df['x'][i])
-        y0.append(df['y'][i])
-        z0.append(df['z'][i])
-        zerr0.append(df['z_err'][i])
+    if fit_mode == 0:
+        if df['flag'][i] == 0:
+            x0.append(df['x'][i])
+            y0.append(df['y'][i])
+            z0.append(df['z'][i])
+            zerr0.append(df['z_err'][i])
+        else:
+            x1.append(df['x'][i])
+            y1.append(df['y'][i])
+            z1.append(df['z'][i])
+            zerr1.append(df['z_err'][i])
     else:
-        x1.append(df['x'][i])
-        y1.append(df['y'][i])
-        z1.append(df['z'][i])
-        zerr1.append(df['z_err'][i])
-
-    # fit_z = calc_fit_z(df['x'][i], df['y'][i])
-    # z_fix = df['z'][i] - fit_z
-    # if df['flag'][i] == 0:
-    #     x0.append(df['x'][i])
-    #     y0.append(df['y'][i])
-    #     z0.append(z_fix)
-    #     zerr0.append(df['z_err'][i])
-    # else:
-    #     x1.append(df['x'][i])
-    #     y1.append(df['y'][i])
-    #     z1.append(z_fix)
-    #     zerr1.append(df['z_err'][i])
+        fit_z = calc_fit_z(df['x'][i], df['y'][i])
+        z_fix = df['z'][i] - fit_z
+        if df['flag'][i] == 0:
+            x0.append(df['x'][i])
+            y0.append(df['y'][i])
+            z0.append(z_fix)
+            zerr0.append(df['z_err'][i])
+        else:
+            x1.append(df['x'][i])
+            y1.append(df['y'][i])
+            z1.append(z_fix)
+            zerr1.append(df['z_err'][i])
 
 def render_frame(angle):
     """data の 3D 散布図を PIL Image に変換して返す"""
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
-    ax.scatter(x0, y0, z0, marker='o', c='r', label='modlule0')
-    for num in np.arange(0, len(zerr0)):
-        ax.plot([x0[num], x0[num]], [y0[num], y0[num]], [z0[num] + zerr0[num], z0[num] - zerr0[num]], marker="_", c='r')
-    ax.scatter(x1, y1, z1, marker='o', c='b', label='modlule1')
-    for num in np.arange(0, len(zerr1)):
-        ax.plot([x1[num], x1[num]], [y1[num], y1[num]], [z1[num] + zerr1[num], z1[num] - zerr1[num]], marker="_", c='b')
+    if mode == 0 or mode == 2:
+        ax.scatter(x0, y0, z0, marker='o', c='r', label='modlule0')
+        for num in np.arange(0, len(zerr0)):
+            ax.plot([x0[num], x0[num]], [y0[num], y0[num]], [z0[num] + zerr0[num], z0[num] - zerr0[num]], marker="_", c='r')
+    if mode == 1 or mode == 2:
+        ax.scatter(x1, y1, z1, marker='o', c='b', label='modlule1')
+        for num in np.arange(0, len(zerr1)):
+            ax.plot([x1[num], x1[num]], [y1[num], y1[num]], [z1[num] + zerr1[num], z1[num] - zerr1[num]], marker="_", c='b')
+    ax.set_xlim([-4.5, 4.5])
+    ax.set_ylim([-4.5, 4.5])
     ax.view_init(5, angle)
     plt.close()
     ax.legend()
