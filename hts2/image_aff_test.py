db9d52398153f40c5e44130479d07d509ef1afe4
Mikimikion <minami.flab@gmail.com>
2023-09-14 02:43:20 +0900

    ファイル名の変更

--- hts2/image_aff_test.py
+++ hts2/image_aff_test.py
@@ -1,7 +1,7 @@
 import cv2
 
-path_img = 'B:\\data\\powerpoint\\GRAINE_nagoya_local\\graine20230530\\fig\\test0-6_adjust.jpg'
-write_path = 'B:\\data\\powerpoint\\GRAINE_nagoya_local\\graine20230530\\fig\\test0-6_adjust_flip.png'
+path_img = 'B:/data/powerpoint/JPS2023/slide/fig/0-6.bmp'
+write_path = 'B:/data/powerpoint/JPS2023/slide/fig/0-6_yfilp.bmp'
 img = cv2.imread(path_img, 0)
 
 img_flip_up = cv2.flip(img, 0)
