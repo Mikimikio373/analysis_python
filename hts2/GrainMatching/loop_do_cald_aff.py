7c6ff9192f653ce4278f560759a364f19677eb40
Mikimikion <minami.flab@gmail.com>
2023-09-15 20:34:51 +0900

    軽微な変更

--- hts2/GrainMatching/loop_do_cald_aff.py
+++ hts2/GrainMatching/loop_do_cald_aff.py
@@ -4,9 +4,10 @@ import subprocess
 basepath = 'Q:\\minami\\202300912_aff'
 python_path = 'c:\\Users\\flab\\analysis_python\\hts2\\GrainMatching\\do_calc_aff.py'
 minus = 45
+mode = 0
 option = '1111'
 ori_path = os.getcwd()
-for i in range(1, 2):
+for i in range(0, 2):
     for j in range(12):
         module = 'Module{}'.format(i)
         sensor = 'sensor-{}'.format(j)
@@ -14,7 +15,7 @@ for i in range(1, 2):
         os.chdir(target_path)
         print('path changed, current path: {}'.format(target_path))
 
-        command_do_calc_aff = 'start python {} {} {}'.format(python_path, minus, option)
+        command_do_calc_aff = 'start python {} {} {} {}'.format(python_path, minus, option, mode)
         print(command_do_calc_aff)
         subprocess.run(command_do_calc_aff, shell=True)
 
