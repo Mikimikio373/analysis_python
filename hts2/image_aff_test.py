f6a6188ea849d1c29115647e4b89b282c53f319d
Mikimikion <minami.flab@gmail.com>
2023-09-15 20:34:38 +0900

    ファイル名の変更

--- hts2/image_aff_test.py
+++ hts2/image_aff_test.py
@@ -1,7 +1,7 @@
 import cv2
 
-path_img = 'B:/data/powerpoint/JPS2023/slide/fig/0-6.bmp'
-write_path = 'B:/data/powerpoint/JPS2023/slide/fig/0-6_yfilp.bmp'
+path_img = 'B:/data/powerpoint/JPS2023/slide/check_ali/0-5.png'
+write_path = 'B:/data/powerpoint/JPS2023/slide/check_ali/0-5_flip.png'
 img = cv2.imread(path_img, 0)
 
 img_flip_up = cv2.flip(img, 0)
