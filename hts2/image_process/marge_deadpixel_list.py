f3699d87620d35371bc9593524351e94b86b6e75
Mikimikion <minami.flab@gmail.com>
2023-09-15 20:40:03 +0900

    deadpixelのリスト情報をjsonファイルにまとめるプログラム

---
+++ hts2/image_process/marge_deadpixel_list.py
@@ -0,0 +1,35 @@
+import json
+import math
+import numpy as np
+
+import pandas as pd
+import os
+import shutil
+
+ori_dir = 'Q:/minami/202300912_aff'
+marge_dir = 'Q:/minami/deadpixel'
+# for module in range(2):
+#     for sensor in range(12):
+#         ori_csv = os.path.join(ori_dir, 'Module{}'.format(module), 'sensor-{}'.format(sensor), 'deadpixel_list.csv')
+#         shutil.copy2(ori_csv, os.path.join(marge_dir, '{}-{:02}_pixel-list.csv'.format(module, sensor)))
+
+ori_json = os.path.join(marge_dir, 'DeadPixel.json')
+with open(ori_json) as f:
+    ori_jsondata = json.load(f)
+
+for i in range(24):
+    module = math.floor(i / 12)
+    sensor = i % 12
+    df = pd.read_csv(os.path.join(marge_dir, '{}-{:02}_pixel-list.csv'.format(module, sensor)))
+    deadpixel_list = []
+    for i in range(len(df)):
+        pair = [float(df['px'][i]), float(df['py'][i])]
+        deadpixel_list.append(pair)
+    print(deadpixel_list)
+    ori_jsondata[i]["DeadPixel"] = deadpixel_list
+
+print(ori_jsondata[0])
+out_json = os.path.join(marge_dir, 'DeadPixel_One_Third.json')
+with open(out_json, 'w') as f:
+    json.dump(ori_jsondata, f)
+
