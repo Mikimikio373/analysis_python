87bc913bf3d582072cc9b03f9df29c55fd37e9cd
Mikimikion <minami.flab@gmail.com>
2023-09-30 15:21:50 +0900

    読み取ったアフィンパラメータを一応出力

--- hts2/GrainMatching/make_affparam.py
+++ hts2/GrainMatching/make_affparam.py
@@ -25,12 +25,13 @@ pq_base = 'Q:/minami/20230914_Ali2/calc_ali'
 
 for i in range(24):
     module = math.floor(i / 12)
-    sensor = 1 % 12
+    sensor = i % 12
     aff = os.path.join(marge_base, '{}-{:02}_aff.csv'.format(module, sensor))
     aff_df = pd.read_csv(aff)
     pq = os.path.join(marge_base, '{}-{:02}_pq.csv'.format(module, sensor))
     pq_df = pd.read_csv(pq)
-    coef = [aff_df['a'][0], aff_df['b'][0], aff_df['c'][0], aff_df['d'][0], pq_df['dx'][0], pq_df['dy'][0]]
+    coef = [aff_df['a'][0], aff_df['b'][0], aff_df['c'][0], aff_df['d'][0], -pq_df['dx'][0], -pq_df['dy'][0]]
+    print(coef)
     affparam[i]["Aff_coef"] = coef
 
 dump = os.path.join(marge_base, 'aff_one_third.json')
