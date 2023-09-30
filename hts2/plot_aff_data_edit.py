766da5d1acdca6b1929e1dfc3f27f5f14eae49f3
Mikimikion <minami.flab@gmail.com>
2023-09-30 15:25:27 +0900

    directory名の変更と、軸ラベルのcommentアウト

--- hts2/plot_aff_data_edit.py
+++ hts2/plot_aff_data_edit.py
@@ -1,14 +1,14 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
-csv_path = 'Q:/minami/202300912_aff/Module0/sensor-3/fitdata_edit.csv'
+csv_path = 'Q:/minami/20230912_aff/Module0/sensor-3/fitdata_edit.csv'
 df = pd.read_csv(csv_path)
 print(df)
 
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 ax.scatter(df['meanX'], df['meanY'], df['dsx'], marker='o', c='r', alpha=1)
-ax.set_xlabel('pixel X [pixel]')
-ax.set_ylabel('pixel Y [pixel]')
-ax.set_zlabel('stage X [mm]')
+# ax.set_xlabel('pixel X [pixel]')
+# ax.set_ylabel('pixel Y [pixel]')
+# ax.set_zlabel('stage X [mm]')
 plt.show()
