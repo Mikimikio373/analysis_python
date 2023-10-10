a6c6615f55b9c66ffe6e942846a94104463d5fe4
Mikimikion <minami.flab@gmail.com>
2023-10-10 14:13:55 +0900

    test

--- hts2/scan_check_tool/test.py
+++ hts2/scan_check_tool/test.py
@@ -1,6 +1,27 @@
 import yaml
+import json
 
-with open('sensor_pos.yml') as f:
-    y = yaml.safe_load(f)
+out_list = []
 
-print(y[0]['module'])
+# for module in range(6):
+#     for sensor in range(12):
+#         tmp = {'module': module, 'sensor': sensor, 'id': module * 12 + sensor, 'pos': 0}
+#         out_list.append(tmp)
+#
+# with open('C:/Users/flab/analysis_python/hts2/scan_check_tool/sensor_pos.yml', 'w') as f:
+#     yaml.safe_dump(out_list, f, default_flow_style=False, sort_keys=False)
+#
+# with open('C:/Users/flab/analysis_python/hts2/scan_check_tool/sensor_pos.json', 'w') as f:
+#     json.dump(out_list, f, indent=2)
+
+with open('C:/Users/flab/analysis_python/hts2/scan_check_tool/sensor_pos.yml', 'rb') as f:
+    y_load = yaml.safe_load(f)
+
+with open('C:/Users/flab/analysis_python/hts2/scan_check_tool/sensor_pos.json', 'w') as f:
+    json.dump(y_load, f, indent=2)
+
+# y_sorted = sorted(y_load, key=lambda x: x['pos'])
+#
+# for i in range(len(y_sorted)):
+#     print(y_sorted[i])
+
