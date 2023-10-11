190b905899f5fd4c16609c7ca202922c62066e6d
Mikimikion <minami.flab@gmail.com>
2023-10-12 06:39:58 +0900

    jsonの読み込みテスト

--- hts2/test.py
+++ hts2/test.py
@@ -1,13 +1,8 @@
-import os
-import shutil
-import glob
-import subprocess
-import pandas as pd
-import matplotlib.pyplot as plt
+import json
 
-python_path = 'C:/Users/flab/analysis_python/hts2/image_process/deadpixel_seach.py'
-for i in range(2):
-    for j in range(12):
-        command = 'start python {} {} {}'.format(python_path, i, j)
-        print(command)
-        subprocess.run(command, shell=True)
+path = 'A:/Test/unit19-8_maxarea/Beta_EachShotParam.json'
+
+with open(path, 'rb') as f:
+    param = json.load(f)
+
+print(len(param))
