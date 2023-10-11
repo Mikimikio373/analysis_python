4ff37df21c8c52c938237d43b84307173d75aa14
Mikimikion <minami.flab@gmail.com>
2023-10-11 12:51:24 +0900

    スキャンエリアのnotのプロット、ほぼ完成、出力ファイル名は用件等

--- hts2/scan_check_tool/plot_not.py
+++ hts2/scan_check_tool/plot_not.py
@@ -1,137 +1,142 @@
 import json
 import sys
-
 import numpy as np
-import pandas as pd
 import matplotlib.pyplot as plt
 import os
-import matplotlib.ticker as mticker
-from matplotlib.colors import Normalize  # Normalizeをimport
+import yaml
+import math
+import copy
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+import itertools
 
-basepath = 'Q:/minami/20230914_300mm240mm'
-not_path = os.path.join(basepath, 'GRAPH', 'NOT')
+step_x = 9.0
+step_y = 4.95
+not_max = 30000
 
-df_not = pd.DataFrame()
-for module in range(2):
-    for sensor in range(12):
-        txt_path = os.path.join(not_path, '{:02}_{:02}_TrackHit2_0_99999999_0_000.txt'.format(module, sensor))
-        if not os.path.exists(txt_path):
-            sys.exit('there are no file: {}'.format(txt_path))
-        f = open(txt_path, 'r')
-        data = f.readlines()
-        df_not['{}-{:02}'.format(module, sensor)] = data
+if not len(sys.argv) == 2:
+    sys.exit('please enter \"target path\"')
 
-not_all = []
-view = 0
-for vy in range(48):
-    not_line_l0_0 = []
-    not_line_l0_1 = []
-    not_line_l0_2 = []
-    not_line_l1 = []
-    for i in range(3):
-        line_tmp0 = []
-        line_tmp1 = []
-        line_tmp2 = []
-        for vx in range(33):
-            line_tmp0.append(int(df_not['1-00'][view].split(' ')[9]))
-            line_tmp0.append(int(df_not['0-08'][view].split(' ')[9]))
-            line_tmp0.append(int(df_not['1-01'][view].split(' ')[9]))
-            line_tmp0.append(int(df_not['0-09'][view].split(' ')[9]))
-            line_tmp0.append(int(df_not['1-02'][view].split(' ')[9]))
-            line_tmp0.append(int(df_not['0-10'][view].split(' ')[9]))
-            line_tmp0.append(int(df_not['1-03'][view].split(' ')[9]))
-            line_tmp0.append(int(df_not['0-11'][view].split(' ')[9]))
+basepath = sys.argv[1]
 
-            line_tmp1.append(int(df_not['1-04'][view].split(' ')[9]))
-            line_tmp1.append(int(df_not['0-04'][view].split(' ')[9]))
-            line_tmp1.append(int(df_not['1-05'][view].split(' ')[9]))
-            line_tmp1.append(int(df_not['0-05'][view].split(' ')[9]))
-            line_tmp1.append(int(df_not['1-06'][view].split(' ')[9]))
-            line_tmp1.append(int(df_not['0-06'][view].split(' ')[9]))
-            line_tmp1.append(int(df_not['1-07'][view].split(' ')[9]))
-            line_tmp1.append(int(df_not['0-07'][view].split(' ')[9]))
 
-            line_tmp2.append(int(df_not['1-08'][view].split(' ')[9]))
-            line_tmp2.append(int(df_not['0-00'][view].split(' ')[9]))
-            line_tmp2.append(int(df_not['1-09'][view].split(' ')[9]))
-            line_tmp2.append(int(df_not['0-01'][view].split(' ')[9]))
-            line_tmp2.append(int(df_not['1-10'][view].split(' ')[9]))
-            line_tmp2.append(int(df_not['0-02'][view].split(' ')[9]))
-            line_tmp2.append(int(df_not['1-11'][view].split(' ')[9]))
-            line_tmp2.append(int(df_not['0-03'][view].split(' ')[9]))
+out_path = os.path.join(basepath, 'GRAPH')
+if not os.path.exists(out_path):
+    os.makedirs(out_path)
 
-            view += 1
-        not_line_l0_0.append(line_tmp0)
-        not_line_l0_1.append(line_tmp1)
-        not_line_l0_2.append(line_tmp2)
-    for i in range(3):
-        not_all.append(not_line_l0_0[i])
-    for i in range(3):
-        not_all.append(not_line_l0_1[i])
-    for i in range(3):
-        not_all.append(not_line_l0_2[i])
+# sensor_exposureをたたいているコマンドから、scan_chech_toolの場所を特定
+pythonpath = os.path.split(sys.argv[0])[0]
+# sensorの場所情報を書いてあるフォルダを読み込み
+with open(os.path.join(pythonpath, 'sensor_pos.yml'), 'rb') as f:
+    y_load = yaml.safe_load(f)
+y_sorted = sorted(y_load, key=lambda x: x['pos'])
 
-    for i in range(3):
-        for vx in range(33):
-            view += 1
+ScanAreaParam = os.path.join(basepath, 'ScanAreaParam.json')
+with open(ScanAreaParam, 'rb') as sap:
+    sap_json = json.load(sap)
 
-x = np.arange(0, 297, 1.125)
-y = np.arange(0, 237.6, 0.55)
-# x, y = np.mgrid[:len(not_all), :len(not_all[0])]
-# print(x)
-# print(y)
-# print(not_all)
-# fig, ax = plt.subplots()
-# z = ax.pcolormesh(x, y, not_all, cmap='jet', vmin=0, vmax=30000)
-# pp = fig.colorbar(z, ax=ax, orientation="vertical")
-# ax.set_xlabel('X [mm]')
-# ax.set_ylabel('Y [mm]')
-# ax.set_aspect('equal')
-#
+with open(os.path.join(basepath, 'ValidViewHistory.json'), 'rb') as f:
+    vvh_json = json.load(f)
+
+sideX = sap_json['SideX']
+sideY = sap_json['SideY']
+layer = sap_json['Layer']
+step_x_num = math.ceil(sideX / step_x)
+step_y_num = math.ceil(sideY / step_y)
+step_y_num3 = step_y_num * 3
+one_third_view = step_x_num * step_y_num
+half_view = step_y_num3 * step_x_num
+view = half_view * layer
+
+# 入れ物用意
+not_l0 = []
+not_l1 = []
+module = 2
+sensor = 12
+for m in range(module):
+    for s in range(sensor):
+        not_tmp_l0 = []
+        not_tmp_l1 = []
+        nottxt_path = os.path.join(basepath, 'DATA', '{:02}_{:02}'.format(m, s), 'TrackHit2_0_99999999_0_000.txt')
+        if not os.path.exists(nottxt_path):
+            sys.exit('there is no file: {}'.format(nottxt_path))
+
+        f = open(nottxt_path, 'r')
+        data_line = f.readlines()
+        for i in range(len(data_line)):
+            # L0, L1の判断
+            if vvh_json[i]['Layer'] == 0:   #L0側
+                not_tmp_l0.append([int(data_line[i].split(' ')[-2]), i])
+            else:
+                not_tmp_l1.append([int(data_line[i].split(' ')[-2]), i])
+        not_l0.append(not_tmp_l0)
+        not_l1.append(not_tmp_l1)
+
+
+not_array_l0 = np.zeros((step_y_num * 9, step_x_num * 8))
+not_array_l1 = np.zeros((step_y_num * 9, step_x_num * 8))
+
+print('all scan area view num: {}'.format(half_view))
+print('scaned area view num: {}'.format(len(not_l0[0])))
+for i in range(half_view):
+    # 1/3でY方向を3分割しているため何レーン目かを判断
+    y_lane = math.floor(i / step_x_num) % 3
+    for py in range(9):
+        for px in range(8):
+            pos = py * 8 + px
+
+            id = y_sorted[pos]['id']
+            if id > 23:
+                continue
+            tmp_l0 = not_l0[id][i]
+            tmp_l1 = not_l1[id][i]
+            #全pcolermesh座標系におけるx,yの計算
+            array_x_l0 = (i % step_x_num) * 8 + px     # xはviewのx座標とpxで計算
+            array_x_l1 = (step_x_num - 1 - (i % step_x_num)) * 8 + px   # l1側はviewの順序を反転
+            array_y = math.floor(i / (step_x_num * 3)) * 9 + py + y_lane    # yはstep_x_numの三倍の商がフルセンサーのview_y + ３回のうち何回目か
+            not_array_l0[array_y][array_x_l0] = tmp_l0[0]
+            not_array_l1[array_y][array_x_l1] = tmp_l1[0]
+
+
+
+cmap = copy.copy(plt.get_cmap("jet"))
+cmap.set_under('w', 1)  # 下限以下の色を設定
+
+
+x = np.arange(step_x_num * 8)
+x = x * step_x / 8
+y = np.arange(step_y_num * 9)
+y = y * step_y / 9
+x, y = np.meshgrid(x, y)
+
+fig = plt.figure(figsize=(11.69, 8.27), tight_layout=True)
+
+ax1 = plt.subplot(221, title='L0 not')
+z_ber0 = ax1.pcolormesh(x, y, not_array_l0, cmap=cmap, vmin=1, vmax=not_max)
+divider0 = make_axes_locatable(ax1) #axに紐付いたAxesDividerを取得
+cax0 = divider0.append_axes("right", size="5%", pad=0.1) #append_axesで新しいaxesを作成
+pp0 = fig.colorbar(z_ber0, orientation="vertical", cax=cax0)
+ax1.set_aspect('equal')
+ax1.set_xlabel('X [mm]')
+ax1.set_ylabel('Y [mm]')
+
+ax2 = plt.subplot(222, title='L1 not')
+z_ber1 = ax2.pcolormesh(x, y, not_array_l1, cmap=cmap, vmin=1, vmax=not_max)
+divider1 = make_axes_locatable(ax2) #axに紐付いたAxesDividerを取得
+cax1 = divider1.append_axes("right", size="5%", pad=0.1) #append_axesで新しいaxesを作成
+pp1 = fig.colorbar(z_ber1, orientation="vertical", cax=cax1)
+ax2.set_aspect('equal')
+ax2.set_xlabel('X [mm]')
+ax2.set_ylabel('Y [mm]')
+
+ax3 = plt.subplot(223, title='L0 not')
+flat_not_l0 = list(itertools.chain.from_iterable(not_array_l0))
+ax3.hist(flat_not_l0, histtype='step', bins=100, range=(0, not_max), color='w', ec='r')
+
+ax4 = plt.subplot(224, title='L1 not')
+flat_not_l1 = list(itertools.chain.from_iterable(not_array_l1))
+ax4.hist(flat_not_l1, histtype='step', bins=100, range=(0, not_max), color='w', ec='b')
+
 # plt.show()
+plt.savefig(os.path.join(out_path, 'not_plot.png'), dpi=300)
 
-df_not_list = pd.DataFrame()
-not_list_sensor = []
-for module in range(2):
-    for sensor in range(12):
-        nog_list = []
-        for i in range(len(df_not['{}-{:02}'.format(module, sensor)])):
-            nog_list.append(int(df_not['{}-{:02}'.format(module, sensor)][i].split(' ')[9]))
 
-        df_not_list['{}-{:02}'.format(module, sensor)] = [np.average(nog_list)]
-        not_list_sensor.append(np.average(nog_list))
-
-x = np.arange(24)
-plt.plot(x, not_list_sensor, 'x')
-plt.ylim(0, 16000)
-plt.xticks(np.arange(0,24))
-plt.grid()
-plt.xlabel('sensor ID')
-plt.ylabel('average not')
-# plt.show()
-plt.clf()
-
-json_path = 'X:/Project_v3/AdminParam/HTS2/SapEVMG/default_HTS.json'
-with open(json_path, 'rb') as f:
-    json_data = json.load(f)
-
-tar_bright = []
-for i in range(24):
-    tar_bright.append(int(json_data['ImagerControllerParamList'][i]['TargetBrightness']))
-
-plt.plot(tar_bright, not_list_sensor, 'x')
-plt.xlabel('taget bright')
-plt.ylabel('average not')
-# plt.show()
-plt.clf()
-
-# fig = plt.figure()
-# ax = fig.add_subplot(111, projection='3d')
-# ax.scatter(x, tar_bright, not_list_sensor)
-# plt.show()
-
-out_df = pd.DataFrame()
-out_df['sensor'] = x + 1
-out_df['not'] = not_list_sensor
-out_df['tar_bright'] = tar_bright
-out_df.to_csv(os.path.join(basepath, 'GRAPH', 'not_list.csv'), index=False)
