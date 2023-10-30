83f7aa7588172f6b59d3b7ce184d5eae615b184a
Mikimikion <minami.flab@gmail.com>
2023-10-30 12:24:52 +0900

    定期更新

--- hts2/test.py
+++ hts2/test.py
@@ -1,8 +1,16 @@
 import json
+import math
 
-path = 'A:/Test/unit19-8_maxarea/Beta_EachShotParam.json'
+ori_path = 'A:/Test/20231026_for_beta_ali/Beta_EachShotParam_org.json'
+path = 'A:/Test/20231026_for_beta_ali/Beta_EachShotParam.json'
 
 with open(path, 'rb') as f:
-    param = json.load(f)
+    sjson = json.load(f)
+with open(ori_path, 'rb') as f:
+    sjson_ori = json.load(f)
 
-print(len(param))
+for i in range(0, len(sjson), 24):
+    print(sjson[i]['View'], sjson_ori[i]['View'])
+
+# for i in range(1764):
+#     print(math.floor(i / 84) * 28 + (i % 28))
