76a32fb94497dc193f9628a9d1103a773f8516ac
Mikimikion <minami.flab@gmail.com>
2023-11-09 17:12:56 +0900

    plotするように変更

--- hts2/scan_check_tool/plot_scanlog.py
+++ hts2/scan_check_tool/plot_scanlog.py
@@ -14,4 +14,10 @@ Hz_list = []
 for i in range(len(l)):
     Hz_list.append(float(l[i].split(',')[1][16:22]))
 
+x = np.arange(len(Hz_list))
+plt.plot(x, Hz_list, 'x', ms=0.2)
+plt.xlabel('Number Of View', fontsize=15)
+plt.ylabel('Frequency [Hz]', fontsize=15)
+plt.grid()
 print(np.mean(Hz_list))
+plt.show()
