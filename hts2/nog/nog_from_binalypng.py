6b5946fd51d18475b7ef6be2cc508c304c5cf4d4
Mikimikion <minami.flab@gmail.com>
2023-12-06 22:43:00 +0900

    二値化画像からヒットピクセル数と、ラベリングしたオブジェクト数をカウントする

--- hts2/nog/nog_from_binalypng.py
+++ hts2/nog/nog_from_binalypng.py
@@ -24,6 +24,7 @@ ny = param['Area'][0]['NViewY']
 l = param['Area'][0]['NLayer']
 
 nog_list = [[], []]
+obj_num = [[], []]
 for layer in range(l):
     for vy in range(ny):
         for vx in range(nx):
@@ -36,11 +37,16 @@ for layer in range(l):
             img = cv2.imread(png_path, 0)
             nog = cv2.countNonZero(img)
             nog_list[layer].append(nog)
+            ret, thr = cv2.threshold(img, 2, 255, cv2.THRESH_BINARY)
+            retval, labels = cv2.connectedComponents(thr)
+            obj_num[layer].append(retval - 1)
         print('vy: {} / {} ended'.format(vy, ny))
 
 out_df = pd.DataFrame()
 out_df['L0'] = nog_list[0]
 out_df['L1'] = nog_list[1]
+out_df['obj_L0'] = obj_num[0]
+out_df['obj_L1'] = obj_num[1]
 out_path = os.path.join(basepath, 'nog_list.csv')
 out_df.to_csv(out_path, index=False)
 
