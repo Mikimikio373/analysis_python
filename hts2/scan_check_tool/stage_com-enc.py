896855c3b6c2b3a7d08ea6ff1e54db4779422b61
Mikimikion <minami.flab@gmail.com>
2023-12-24 15:13:27 +0900

    汎用化。カレントディレクトリでたたくように変更。

--- hts2/scan_check_tool/stage_com-enc.py
+++ hts2/scan_check_tool/stage_com-enc.py
@@ -1,23 +1,26 @@
 import json
 import os.path
+from statistics import mean
 import sys
 
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_pdf import PdfPages
 
 
-def plot_vec(fig, pdf: PdfPages, X: list, Y: list, U: list, V: list, title: str, factor: float, *, guide: float = 0.001):
+def plot_vec(fig, pdf: PdfPages, X: list, Y: list, U: list, V: list, title: str, factor: float, *, width :float = 1, guide: float = 0.001, min_xrange: float = 18):
     ax = fig.add_subplot(111)
-    ax.quiver(X, Y, U, V, angles='xy', scale_units='xy', scale=1, units='xy', width=0.5)
+    ax.quiver(X, Y, U, V, angles='xy', scale_units='xy', scale=1, units='xy', width=width)
     x = min(X) - (max(X) - min(X)) * 0.05
     y = min(Y) - (max(Y) - min(Y)) * 0.05
-    ax.quiver(x, y, guide * factor, 0, angles='xy', scale_units='xy', scale=1, units='xy', width=0.4)
-    ax.quiver(x, y, 0, guide * factor, angles='xy', scale_units='xy', scale=1, units='xy', width=0.4)
+    ax.quiver(x, y, guide * factor, 0, angles='xy', scale_units='xy', scale=1, units='xy', width=width)
+    ax.quiver(x, y, 0, guide * factor, angles='xy', scale_units='xy', scale=1, units='xy', width=width)
     ax.text(x, y, f" {guide * 1000} um", va="bottom", ha="left")
-    ax.set_aspect(1)
+    if max(X) - min(X) < min_xrange:
+        ax.set_xlim(mean(X) - min_xrange, mean(X) + min_xrange)
     ax.set_title(title)
     ax.set_xlabel("Stage X [mm]")
     ax.set_ylabel("Stage Y [mm]")
+    ax.set_aspect('equal')
     pdf.savefig()
     plt.clf()
 
@@ -38,11 +41,13 @@ def plot_stage_shift_scatter(fig, pdf, X: list, Y: list, U: list, V: list, title
             plt.clf()
             i += 1
 
-if len(sys.argv) != 3:
-    sys.exit('exception: usage .py [input path], [output path]')
+# if len(sys.argv) != 3:
+#     sys.exit('exception: usage .py [input path], [output path]')
 
-basepath = sys.argv[1]
-out_dir = sys.argv[2]
+# basepath = sys.argv[1]
+# out_dir = sys.argv[2]
+
+basepath = os.getcwd()
 
 path = os.path.join(basepath, 'ValidViewHistory.json')
 
@@ -53,10 +58,10 @@ print('open: {}'.format(path))
 with open(path, 'rb') as f:
     vvh = json.load(f)
 
-if not os.path.exists(out_dir):
-    sys.exit('there are no dir: {}'.format(out_dir))
+# if not os.path.exists(out_dir):
+#     sys.exit('there are no dir: {}'.format(out_dir))
 
-pdf_path = os.path.join(out_dir, '{}_pos.pdf'.format(os.path.basename(basepath)))
+pdf_path = os.path.join(basepath, '{}_pos.pdf'.format(os.path.basename(basepath)))
 print('open to write: {}'.format(pdf_path))
 pdf = PdfPages(pdf_path)
 
@@ -81,6 +86,7 @@ for i in range(len(vvh)):
 
 factor = 10000
 guide = 0.001
+width = 0.5
 
 fig = plt.figure()
 
@@ -91,10 +97,10 @@ for i in range(2):
         Y = command[i]['Y']
         U = [(x - y) * factor for (x, y) in zip(pos[i]['X'], command[i]['X'])]
         V = [(x - y) * factor for (x, y) in zip(pos[i]['Y'], command[i]['Y'])]
-        plot_vec(fig, pdf, X, Y, U, V, title, factor)
+        plot_vec(fig, pdf, X, Y, U, V, title, factor, width=width)
 
         U = [(x - y) * 1000 for (x, y) in zip(pos[i]['X'], command[i]['X'])]
         V = [(x - y) * 1000 for (x, y) in zip(pos[i]['Y'], command[i]['Y'])]
-        plot_stage_shift_scatter(fig, pdf, X, Y, U, V, title, ymin=-2, ymax=2)
+        plot_stage_shift_scatter(fig, pdf, X, Y, U, V, title, ymin=-1.5, ymax=1.5)
 
 pdf.close()
