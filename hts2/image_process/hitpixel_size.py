50520970ba7b8bee6cf1fe069fc988a54faa9abf
Mikimikion <minami.flab@gmail.com>
2024-01-17 19:45:04 +0900

    hitpixelサイズを計算するプログラム

--- hts2/image_process/hitpixel_size.py
+++ hts2/image_process/hitpixel_size.py
@@ -6,30 +6,43 @@ import cv2
 import matplotlib.pyplot as plt
 
 # basepath = 'Q:/minami/20231204_fakeimg/IMAGE00_AREA-1-0.06/png/L0_VX0000_VY0000'
-# basepath = 'R:/usuda/GRAINE2023_u4/PL088_0904gap4/IMAGE00_AREA-1/png_thr_nogdilate/png_thr10_9/L0_VX0000_VY0000'
-basepath = 'Q:/minami/20220429_suganami/006/IMAGE00_AREA-1/png_thr_nolilate/png_thr10_9/L0_VX0000_VY0000'
+basepath = 'R:/usuda/GRAINE2023_u4/PL088_0904gap4/IMAGE00_AREA-1/png_thr_nondilate/png_thr10_9'
+basepath_cubic = 'R:/usuda/GRAINE2023_u4/PL088_0904gap4/IMAGE00_AREA-1/png_thr_nondilate/png_thr_cubic10_9_zfilt-0.15'
+# basepath = 'Q:/minami/20220429_suganami/006/IMAGE00_AREA-1/png_thr_nolilate/png_thr10_9/L0_VX0000_VY0000'
 
-# for i in range(0, 1):
-img_path = os.path.join(basepath, 'L0_VX0000_VY0000_{}.png'.format(11))
-img = cv2.imread(img_path, 0)
-ret, img = cv2.threshold(img, 100, 255, cv2.THRESH_BINARY)
-retval, labels, stats, centor_ids = cv2.connectedComponentsWithStats(img)
-print(retval, np.count_nonzero(img))
-print(np.count_nonzero(img) / (2048 * 1088))
+picnum = 11
+stepx = 27
+stepy = 55
+all_step = stepx * stepy
 
-stats = np.asarray(np.delete(stats, 0, axis=0))
-area = []
-for stat in stats:
-    area.append(stat[4])
+area = [[], []]
+for vx in range(stepx):
+    for vy in range(stepy):
+        for area_pos, path in enumerate([basepath, basepath_cubic]):
+            img_path = os.path.join(path, 'L0_VX{:04}_VY{:04}'.format(vx, vy), 'L0_VX{:04}_VY{:04}_{}.png'.format(vx, vy, picnum))
+            img = cv2.imread(img_path, 0)
+            ret, img = cv2.threshold(img, 100, 255, cv2.THRESH_BINARY)
+            retval, labels, stats, centor_ids = cv2.connectedComponentsWithStats(img)
+            stats = np.asarray(np.delete(stats, 0, axis=0))
+            area[area_pos] = np.hstack([area[area_pos], stats[:, 4]])  # hstack:横に結合  stats[:, 4]列の抽出(面積のとこだけ抜き出し)
+    print('vx-{}/{} ended'.format(vx, stepx))
 
-plt.hist(area, bins=100, range=(0.5, 100.5), log=True)
-plt.show()
 
-# test = np.random.rand(9, 9)
-# ret, test = cv2.threshold(test, 0.05, 255, cv2.THRESH_BINARY_INV)
-# cv2.imshow('test', test)
-# cv2.waitKey(0)
-#
-# test = cv2.dilate(test, np.ones((2, 2), np.uint8))
-# cv2.imshow('test', test)
-# cv2.waitKey(0)
+print(np.sum(area[0]) / all_step)
+print(np.sum(area[1]) / all_step)
+factor = 1 / all_step
+plt.figure(tight_layout=True)
+counts, bins = np.histogram(area[0], bins=10, range=(0.5, 10.5))
+plt.hist(bins[:-1], bins, edgecolor='r', hatch='//', histtype='step', weights=factor*counts, label='Used')
+counts, bins = np.histogram(area[1], bins=10, range=(0.5, 10.5))
+plt.hist(bins[:-1], bins, edgecolor='b', hatch='\\\\', histtype='step', weights=factor*counts, label='Not used')
+plt.legend()
+plt.xticks(range(1, 11), fontsize=14)
+plt.yticks(fontsize=14)
+plt.xlabel('Area of hitpixels', fontsize=18, loc='right')
+plt.ylabel('Entries', fontsize=18, loc='top')
+# plt.show()
+savepath = 'B:/data/powerpoint/HTS2_data/4master_theisis/hitpixel_area_comparea.png'
+plt.savefig(savepath, dpi=300)
+
+
