88c795424f96ebf025fbf5f4d5044960ac39b70b
Mikimikion <minami.flab@gmail.com>
2024-02-11 15:23:37 +0900

    スキャンスケジュール。バグ修正

--- hts2/plot_schedule.py
+++ hts2/plot_schedule.py
@@ -1,9 +1,12 @@
+import sys
+
 import numpy as np
 import datetime
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 import pandas as pd
 
+# スキャン予定
 date_list = []
 scan_num_list = []
 scan_num = 0
@@ -53,18 +56,41 @@ labels_y = ax.get_yticklabels()
 plt.setp(labels_x, rotation=45, fontsize=12)
 plt.setp(labels_y, fontsize=12)
 plt.savefig('B:/data/powerpoint/HTS2_data/4master_theisis/scan_schedule/pred.png', dpi=300)
+plt.clf()
 # plt.show()
 
+# 実績と予定
+df = pd.read_csv('B:/data/powerpoint/HTS2_data/4master_theisis/scan_schedule/scan_prog_20240210.csv', header=None)
+date_list_result = pd.to_datetime(df[0].values)
+scan_num_list_result = df[1].values
+# 実績の日付が開いているところを補正一日ごとになるように
+res_start = date_list_result[0]
+res_end = date_list_result[-1]
+date = res_start
+index_num = 0
+date_list1 = []
+scan_num_list1 = []
+while date <= res_end:
+    if len(date_list1) == 0:
+        date_list1.append(date)
+        scan_num_list1.append(scan_num_list_result[index_num])
+        date += datetime.timedelta(days=1)
+        index_num += 1
+    elif date == date_list_result[index_num]:
+        date_list1.append(date)
+        scan_num_list1.append(scan_num_list_result[index_num])
+        date += datetime.timedelta(days=1)
+        index_num += 1
+    else:
+        date_list1.append(date)
+        scan_num_list1.append(scan_num_list_result[index_num-1])
+        date += datetime.timedelta(days=1)
 
-df = pd.read_csv('B:/data/powerpoint/HTS2_data/4master_theisis/scan_schedule/scan_prog_20240118.csv', header=None)
-
-date_list1 = pd.to_datetime(df[0].values)
-scan_num_list1 = df[1].values
 scan_num = scan_num_list1[-1]
 date = date_list1[-1]
 scan_num_list2 = []
 date_list2 = []
-endtime1 = datetime.datetime.strptime('2024-1-31', '%Y-%m-%d')
+endtime1 = datetime.datetime.strptime('2024-3-1', '%Y-%m-%d')
 while date < endtime1:
     scan_num += 45.0 / 7
     scan_num_list2.append(scan_num)
@@ -79,18 +105,17 @@ while scan_num <= 1800:
     date += datetime.timedelta(days=1)
 
 print(max(scan_num_list2))
-
+print(date_list2[-1])
 
 height1, x1, ret1 = plt.hist(date_list1, bins=len(date_list1))
-height2, x2, ret2 = plt.hist(date_list2, bins=len(date_list2))
+# height2, x2, ret2 = plt.hist(date_list2, bins=len(date_list2))
 plt.clf()
 fig = plt.figure(tight_layout=True)
 ax = fig.add_subplot(111)
 ax.hist(x1[:-1], bins=x1, weights=scan_num_list1, edgecolor='b', hatch='\\\\', histtype='step')
-ax.hist(x2[:-1], bins=x2, weights=scan_num_list2, edgecolor='r', hatch='\\\\', histtype='step')
+# ax.hist(x2[:-1], bins=x2, weights=scan_num_list2, edgecolor='r', hatch='\\\\', histtype='step')
 ax.xaxis.set_major_locator(mdates.MonthLocator())
 ax.xaxis.set_major_formatter(mdates.DateFormatter('%Y/%m/%d'))
-ax.set_ylim(0, 1800)
 ax.set_ylabel('合計読み取り枚数', fontname="MS Gothic", fontsize=12)
 labels_x = ax.get_xticklabels()
 labels_y = ax.get_yticklabels()
@@ -98,3 +123,7 @@ plt.setp(labels_x, rotation=45, fontsize=12)
 plt.setp(labels_y, fontsize=12)
 plt.savefig('B:/data/powerpoint/HTS2_data/4master_theisis/scan_schedule/result.png', dpi=300)
 # plt.show()
+
+ax.plot(date_list2, scan_num_list2, c='b', ls='--', alpha=0.7)
+ax.set_ylim(0, 1800)
+plt.savefig('B:/data/powerpoint/HTS2_data/4master_theisis/scan_schedule/future.png', dpi=300)
