f2bb101ca4a474c558167f207c41a88125add245
Mikimikion <minami.flab@gmail.com>
2024-02-16 15:15:05 +0900

    膨張処理テストプログラム

--- hts2/image_process/check_dilate.py
+++ hts2/image_process/check_dilate.py
@@ -4,8 +4,8 @@ import numpy as np
 img = np.zeros((8, 8), np.uint8)
 print(img)
 
-for i in range(2, 4):
-    for j in range(2, 4):
+for i in range(2, 3):
+    for j in range(2, 3):
         img[i][j] = 1
 # img[2][2] = 255
 print(img)
@@ -14,3 +14,5 @@ print(img)
 
 img_dilate = cv2.dilate(img, np.ones((3, 3), np.uint8))
 print(img_dilate)
+img_erode = cv2.erode(img_dilate, np.ones((3, 3), np.uint8))
+print(img_erode)
