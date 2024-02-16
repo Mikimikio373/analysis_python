a2088ab487a3f3ab490a1b83345771e6988f0708
Mikimikion <minami.flab@gmail.com>
2024-02-16 15:57:20 +0900

    二値化画像からnog数を数えるプログラム

--- hts2/nog/nog_from_binalypng.py
+++ hts2/nog/nog_from_binalypng.py
@@ -1,3 +1,4 @@
+import json
 import os.path
 import sys
 
@@ -6,12 +7,14 @@ import cv2
 import pandas as pd
 
 
-if not len(sys.argv) == 4:
-    sys.exit('command line error. please input [basepath], [AreaScanParam_path], [picnum]')
+if not len(sys.argv) == 6:
+    sys.exit('command line error. please input [basepath], [jsonpath], [AreaScanParam_path], [npicturenum], [fakeflag]')
 
 basepath = sys.argv[1]
-yaml_path = sys.argv[2]
-picnum = int(sys.argv[3])
+json_path = sys.argv[2]
+yaml_path = sys.argv[3]
+npic = int(sys.argv[4])
+fakeflag = int(sys.argv[5])
 
 with open(yaml_path, 'rb') as f:
     param = yaml.safe_load(f)
@@ -22,22 +25,38 @@ l = param['Area'][0]['NLayer']
 
 nog_list = [[], []]
 obj_num = [[], []]
-for layer in range(l):
-    for vy in range(ny):
+view = 0
+for vy in range(ny):
+    for layer in range(l):
         for vx in range(nx):
             fname = 'L{}_VX{:04}_VY{:04}'.format(layer, vx, vy)
-            png_path = os.path.join(basepath, fname, fname + '_{}.png'.format(picnum))
+            if not fakeflag == 1:
+                with open(os.path.join(json_path, 'V{:08}_L{}_VX{:04}_VY{:04}_0_{:03}.json'.format(view, layer, vx, vy, npic)), 'rb') as f:
+                    j = json.load(f)
+                if layer == 1: # stage側
+                    first = j['First'] - 1
+                    last = first + 15
+                else: # lens側
+                    last = j['Last'] - 1
+                    first = last - 15
+            else:
+                first = 0
+                last = 15
+            for picnum in range(first, last + 1):
+                png_path = os.path.join(basepath, fname, fname + '_{}.png'.format(picnum))
 
-            if not os.path.exists(png_path):
-                sys.exit('There is no file: {}'.format(png_path))
+                if not os.path.exists(png_path):
+                    sys.exit('There is no file: {}'.format(png_path))
 
-            img = cv2.imread(png_path, 0)
-            nog = cv2.countNonZero(img)
-            nog_list[layer].append(nog)
-            ret, thr = cv2.threshold(img, 2, 255, cv2.THRESH_BINARY)
-            retval, labels = cv2.connectedComponents(thr)
-            obj_num[layer].append(retval - 1)
-        print('vy: {} / {} ended'.format(vy, ny))
+                img = cv2.imread(png_path, 0)
+                nog = cv2.countNonZero(img)
+                nog_list[layer].append(nog)
+                ret, thr = cv2.threshold(img, 2, 255, cv2.THRESH_BINARY)
+                retval, labels = cv2.connectedComponents(thr)
+                obj_num[layer].append(retval - 1)
+
+            view += 1
+    print('vy: {} / {} ended'.format(vy, ny))
 
 out_df = pd.DataFrame()
 out_df['L0'] = nog_list[0]
